fb9cdcfebaad36b859971b8b7fab6c0d7e65918e
ItotiaHarrison <harrison.itotia@student.moringaschool.com>
2022-09-29 01:48:09 +0300

    create hook demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('world');
+
+  useEffect(() => {
+    document.title = `Hello, ${name}`;
+  });
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello, {name}!</h1>
+      <button onClick={() => setName('Harrison')}>
+        Click me to change the name
+      </button>
     </div>
   );
 }
